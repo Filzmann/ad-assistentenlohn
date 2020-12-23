fc3a8b2a305ac52cebfd8bf13bf4696d710db494
Simon Beyer <simonbeyer79@gmail.com>
2020-12-23 19:29:49 +0100

    Stand 22.12.2020 20:34

--- fenster_verpflegungsmehraufwand.py
+++ fenster_verpflegungsmehraufwand.py
@@ -7,7 +7,7 @@ from person import AS
 class FensterVerpflegungsMehraufwand(tk.Toplevel):
     class TabelleVerpflegungsMehraufwand(tk.Frame):
         def __init__(self, parent, assistent: AS, jahr: tk.IntVar):
-            super().__init__(parent, bg='red', height=10, width=100 )
+            super().__init__(parent)
             self.assistent = assistent
             self.jahr = jahr.get()
             self.draw()
@@ -17,7 +17,10 @@ class FensterVerpflegungsMehraufwand(tk.Toplevel):
             end = datetime.datetime(self.jahr + 1, 1, 1) - datetime.timedelta(seconds=1)
             schichten = self.assistent.get_all_schichten(start=start, end=end)
             # TODO Randschichten prüfen, ob sie in anderes Jahr gehören (größter Anteil Stunden)
-            steuerarray = {"über 24":0, "über 8": 0, "unter 8": 0}
+            steuerarray = {"Abwesenheit über 24 Stunden": 0,
+                           "An-/Abreisetag": 0,
+                           "Abwesenheit über 8 Stunden": 0,
+                           "Abwesenheit unter 8 Stunden": 0}
             for schicht in schichten:
                 beginnadresse = endadresse = schichten[schicht].asn.home
                 if schichten[schicht].beginn_andere_adresse:
@@ -29,21 +32,28 @@ class FensterVerpflegungsMehraufwand(tk.Toplevel):
                 abwesenheit += datetime.timedelta(minutes=zeit_hinfahrt)
                 zeit_rueckfahrt = self.assistent.get_fahrtzeit(self.assistent.home, endadresse)
                 abwesenheit += datetime.timedelta(minutes=zeit_rueckfahrt)
-                if abwesenheit.seconds/3600 > 24:
-                    steuerarray['über 24'] += 1
-                elif abwesenheit.seconds/3600 > 8:
-                    steuerarray['über 8'] += 1
+                if abwesenheit.seconds / 3600 > 24:
+                    if schicht.teilschichten:
+                        for teilschicht in schicht.teilschichten:
+                            if (teilschicht.ende - teilschicht.beginn).seconds / 3600 == 24:
+                                steuerarray['Abwesenheit über 24 Stunden'] += 1
+                            else:
+                                steuerarray['An-/Abreisetag'] += 1
+                    else:
+                        steuerarray['Abwesenheit über 24 Stunden'] += 1
+                elif abwesenheit.seconds / 3600 > 8:
+                    steuerarray['Abwesenheit über 8 Stunden'] += 1
                 else:
-                    steuerarray['unter 8'] += 1
+                    steuerarray['Abwesenheit unter 8 Stunden'] += 1
 
             zeilennummer = 0
             for zeile in steuerarray:
                 label = tk.Label(self, text=zeile)
-                label.grid(row=zeilennummer, column=0)
+                label.grid(row=zeilennummer, column=0, sticky="w")
                 entry = tk.Label(self, text=steuerarray[zeile])
-                entry.grid(row=zeilennummer, column=1)
+                entry.grid(row=zeilennummer, column=1, sticky="e")
                 zeilennummer += 1
-
+                self.grid()
 
             return schichten
 
@@ -58,7 +68,7 @@ class FensterVerpflegungsMehraufwand(tk.Toplevel):
         self.selected_year = tk.IntVar()
         self.selected_year.set(heute.year)
         self.jahr_dropdown = tk.OptionMenu(self, self.selected_year, *jahre, command=self.change_jahr)
-        self.tabelle_vma = self.TabelleVerpflegungsMehraufwand(parent, assistent, self.selected_year)
+        self.tabelle_vma = self.TabelleVerpflegungsMehraufwand(self, assistent, self.selected_year)
         self.change_jahr(self.selected_year.get())
         # ins Fenster packen
         self.headline.grid(row=0, column=0)
